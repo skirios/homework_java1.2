34d951b27f7a7788ffc6c90421cfd5d50ec83e3d
skirios <skiriosfoto@gmail.com>
2021-03-05 07:48:09 +0300

    added report.md

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "2204771585322443";
+        String number = "4916693280984037";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
